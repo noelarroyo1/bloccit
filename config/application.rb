ae0b62c240bb4e63ab083781bb32be207ae73149
Noel Arroyo <noel.arroyo1@gmail.com>
2018-03-15 21:45:07 -0400

    add random data and drop them seeds

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,6 @@ module Bloccit
     # -- all .rb files in that directory are automatically loaded.
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.autoload_paths << File.join(config.root, "lib")
   end
 end
