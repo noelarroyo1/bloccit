03d35dc22125ea7e15c944cf60adc1b89e2330b9
Noel Arroyo <noel.arroyo1@gmail.com>
2018-03-23 17:11:46 -0400

    assignment done

--- spec/controllers/advertisement_controller_spec.rb
+++ spec/controllers/advertisement_controller_spec.rb
@@ -27,6 +27,16 @@ RSpec.describe AdvertisementController, type: :controller do
       get :new
       expect(response).to have_http_status(:success)
     end
+
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    it "instantiates @advertisement" do
+      get :new
+      expect(assigns(:advertisement)).not_to be_nil
+    end
   end
 
   describe "GET #create" do
@@ -35,5 +45,21 @@ RSpec.describe AdvertisementController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "POST create" do
+    it "increases the number of advertisements by 1" do
+      expect{ post :create, advertisement: { title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 10 } }.to change(Advertisement, :count).by(1)
+    end
+
+    it "assigns the new advertisement to @advertisement" do
+      post :create, advertisement: { title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 10 }
+      expect(assigns(:post)).to eq Advertisement.last
+    end
+
+   it "redirects to the new advertisement" do
+      post :create, advertisement: { title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 10 }
+      expect(response).to redirect_to Advertisement.last
+   end
+ end
 
 end
