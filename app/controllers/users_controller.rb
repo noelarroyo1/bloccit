f842dc9c4c193c3054d2d2f65e777e54fb069f66
Noel Arroyo <noel.arroyo1@gmail.com>
2018-04-04 20:15:15 -0400

    add confirmation page and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def confirm
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  helper_method :user_params
 end
