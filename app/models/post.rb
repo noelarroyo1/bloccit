5079f991fb5ff672d55818b0f4436eab58f8427e
Noel Arroyo <noel.arroyo1@gmail.com>
2018-04-10 10:55:52 -0400

    fix backwards logic on scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ApplicationRecord
 # in descending order, with the most recent posts displayed first.
   default_scope { order('created_at DESC') }
   scope :ordered_by_title, -> { order(title: :asc) }
-  scope :ordered_by_reverse_created_at, -> { order('created_at DESC') }
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
